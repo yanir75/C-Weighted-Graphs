2b1f677cc2ab2c06ea5efa65b24b4fe34ba889f3
Yanir <66729230+yanir75@users.noreply.github.com>
2022-01-03 21:15:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "graph.h"
+typedef struct edge{
+    int weight;
+    struct node* dest;
+    struct edge *next_edge;
+}edge;
+
+typedef struct node{
+    int id;
+    struct node *next;
+    edge *edges;
+}node;
+
+typedef struct queue{
+    int prio;
+    node *info;
+    struct queue *next;
+}priorityQueue;
 
 void print(node *head){
     while(head!=NULL){
